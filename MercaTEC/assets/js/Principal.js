81362a4dc1e9b9ca0e29c1fa96a1d329934bb582
David Acuña <89756491+fiito720@users.noreply.github.com>
2024-05-06 11:32:35 -0600

    Contraste

--- MercaTEC/assets/js/Principal.js
+++ MercaTEC/assets/js/Principal.js
@@ -210,4 +210,15 @@ function Comprar(elemento){
             });
         })
     }
+};
+
+
+function setAltoContraste(){
+    var stylesheet = document.getElementById('altoContrasteCss');
+    stylesheet.disabled = !stylesheet.disabled;
+    localStorage.setItem('ALTOCONTRASTE', stylesheet.disabled ? 0 : 1);
+
+    var stylesheet = document.getElementById('altoContrasteIndexCss');
+    stylesheet.disabled = !stylesheet.disabled;
+    localStorage.setItem('ALTOCONTRASTE', stylesheet.disabled ? 0 : 1);
 }
